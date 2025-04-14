a7e1433a71fe50ef3706037c2eee1ef69899ea30
Ygimenez <yagogimenez1@hotmail.com>
2025-04-14 10:59:54 -0300

    Fixed wrong GUID declaration.

--- bin/templates.dart
+++ bin/templates.dart
@@ -14,7 +14,7 @@ const kInstaller = r'''
 [Setup]
 ; NOTE: The value of AppId uniquely identifies this application. Do not use the same AppId value in installers for other applications.
 ; (To generate a new GUID, click Tools | Generate GUID inside the IDE.)
-AppId={{{GUID}}}
+AppId={{GUID}}
 AppName={#MyAppName}
 AppVersion={#MyAppVersion}
 ; AppVerName={#MyAppName} {#MyAppVersion}
